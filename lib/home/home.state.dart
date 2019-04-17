2961ffea1bae9c69b22be978bd5470a177d78f0b
Rafael Passarela <rafaelpassarela@gmail.com>
2019-04-16 21:32:41 -0300

    Gradient background

--- lib/home/home.state.dart
+++ lib/home/home.state.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:parrot_pronunciation_app/widgets/tts.controller.dart';
+import 'package:parrot_pronunciation_app/widgets/circular.button.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:parrot_pronunciation_app/localization/localization.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,6 +24,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   int _counter = 0;
   bool _canSpeak = false;
 
@@ -30,28 +34,20 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    textControllerInputWord.addListener( () {
-      bool isEmpty = textControllerInputWord.text.isEmpty;
-
-      // if you can speak, but the new text is invalid
-      // or if you can't speak but the text is valid, refresh the state
-      if ((_canSpeak && isEmpty) || ( !_canSpeak && !isEmpty) ) {
-        setState(() {
-          _canSpeak = !isEmpty;
-        });
-      }
-    });
+    textControllerInputWord.addListener(_inputTextChangeListener);
   }
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    _counter++;
+    Fluttertoast.showToast(
+      msg: "Total Clicks: " + _counter.toString(),
+      toastLength: Toast.LENGTH_LONG,
+      //gravity: ToastGravity.CENTER,
+      //timeInSecForIos: 2,
+      //backgroundColor: Colors.red,
+      //textColor: Colors.white,
+      //fontSize: 16.0
+    );
   }
 
   @override
@@ -73,53 +69,61 @@ class _HomePageState extends State<HomePage> {
         )
       ),
 
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _buildInputTextField(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            // add additional colors to define a multi-point gradient
+            colors: [
+              Colors.white,
+              Color(0x4089ED91)],
+          ),
+        ),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildInputTextField(),
 
-            RaisedButton(
-              onPressed: (_canSpeak) ? _speechText : null,
-              child: Icon(Icons.volume_up),
+              RaisedButton(
+                onPressed: (_canSpeak) ? _speechText : null,
+                child: Icon(Icons.volume_up),
               ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            IconButton(
-              icon: Icon(Icons.mic),
-              onPressed: null,
-            ),
-            IconButton(
-              icon: Icon(Icons.forum),
-              onPressed: null,
-            ),
-
-          ],
+              IconButton(
+                icon: Icon(Icons.mic),
+                onPressed: null,
+              ),
+              IconButton(
+                icon: Icon(Icons.forum),
+                onPressed: null,
+              ),
+//            CircularButton(),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 
@@ -128,20 +132,30 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.all(20),
       child: TextField(
         autofocus: true,
-        maxLines: null, // maxLines = NULL: adds new lines when the current line reaches the line character limit
+        // maxLines = NULL: adds new lines when the current line reaches the line character limit
+        maxLines: null,
         cursorColor: Colors.green,
         controller: textControllerInputWord,
         decoration: InputDecoration(
           labelText: LocalizationController.of(context).inputWord,
           helperText: LocalizationController.of(context).inputHint,
-          //border: OutlineInputBorder(),
+          border: OutlineInputBorder(),
         ),
-
       ),
     );
   }
 
-  void _speechText() {
+  void _inputTextChangeListener() {
+    bool isEmpty = textControllerInputWord.text.isEmpty;
 
+    // if you can speak, but the new text is invalid
+    // or if you can't speak but the text is valid, refresh the state
+    if ((_canSpeak && isEmpty) || (!_canSpeak && !isEmpty)) {
+      setState(() {
+        _canSpeak = !isEmpty;
+      });
+    }
   }
+
+  void _speechText() {}
 }
